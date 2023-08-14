950052384bf46aa9b713261c2a8d9e779240592d
tuvsheezz <tuvshee0089@gmail.com>
2023-08-14 19:03:37 +0900

    ITP1-Topic2

---
+++ 1.ITP1/Topic2/ITP1_2_D.py
@@ -0,0 +1,10 @@
+w, h, x, y, r = map(int, input().split())
+xmin = x - r
+xmax = x + r
+ymin = y - r
+ymax = y + r
+
+if 0 <= xmin and xmax <= w and 0 <= ymin and ymax <= h:
+    print('Yes')
+else:
+    print('No')
