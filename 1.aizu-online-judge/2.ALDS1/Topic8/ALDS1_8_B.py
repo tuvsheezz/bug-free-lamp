d27aa9e7d9f68ad807e65071848a0df60389fa93
tuvsheezz <tuvshee0089@gmail.com>
2024-01-01 16:27:56 +0900

    binary search tree: find

---
+++ 1.aizu-online-judge/2.ALDS1/Topic8/ALDS1_8_B.py
@@ -0,0 +1,84 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.parent = None
+        self.left = None
+        self.right = None
+
+
+class BST:
+    def __init__(self):
+        self.root = None
+        self.ino = None
+        self.preo = None
+
+    def insert(self, z):
+        x = self.root
+        y = None
+        while x is not None:
+            y = x
+            if z.data < x.data:
+                x = x.left
+            else:
+                x = x.right
+            z.parent = y
+
+        z.parent = y
+
+        if y is None:
+            self.root = z
+        elif z.data < y.data:
+            y.left = z
+        else:
+            y.right = z
+
+    def find(self, root, k):
+        if root is None:
+            return False
+        if root.data == k:
+            return True
+        elif k < root.data:
+            return self.find(root.left, k)
+        else:
+            return self.find(root.right, k)
+
+    def inorder(self, root):
+        if root is None:
+            return
+
+        self.inorder(root.left)
+        self.ino.append(root.data)
+        self.inorder(root.right)
+
+    def preorder(self, root):
+        if root is None:
+            return
+
+        self.preo.append(root.data)
+        self.preorder(root.left)
+        self.preorder(root.right)
+
+    def print(self):
+        self.ino = list()
+        self.preo = list()
+        self.inorder(self.root)
+        print(" " + " ".join(map(str, self.ino)))
+        self.preorder(self.root)
+        print(" " + " ".join(map(str, self.preo)))
+
+
+if __name__ == "__main__":
+    n = int(input())
+    bst = BST()
+
+    for _ in range(n):
+        com, *val = input().split()
+        if com == "insert":
+            bst.insert(Node(int(val[0])))
+        elif com == "print":
+            bst.print()
+        elif com == "find":
+            if bst.find(bst.root, int(val[0])):
+                print("yes")
+            else:
+                print("no")
